3ffd734b9c7ea0eda4794f21fd6d596d0a2261fd
gilbert-khonstantine <khonstantine@gmail.com>
2021-02-22 11:10:40 +0800

    first commit

---
+++ src/main/java/com/gideon/realtimetextcall/RealtimeTextCallApplication.java
@@ -0,0 +1,13 @@
+package com.gideon.realtimetextcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeTextCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeTextCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gideon/realtimetextcall/RealtimeTextCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gideon.realtimetextcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeTextCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
