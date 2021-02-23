25ba4bbc8652163d12cf3274c69f8b038104c5f0
gilbert-khonstantine <khonstantine@gmail.com>
2021-02-23 11:43:46 +0800

    lagging if 2 person edits at the same time

--- src/main/java/com/gideon/realtimetextcall/Controller/DocumentController.java
+++ src/main/java/com/gideon/realtimetextcall/Controller/DocumentController.java
@@ -27,6 +27,7 @@ public class DocumentController {
     public ChatMessage register(@Payload Map<String, String> payload, @DestinationVariable String roomId,SimpMessageHeaderAccessor headerAccessor) {
         ChatMessage chatMessage = new ChatMessage(payload.get("sender"), payload.get("content"));
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        System.out.println(payload.get("content"));
         if (payload.containsKey("roomId")){
             Room room = new Room(Long.parseLong(payload.get("roomId")), payload.get("roomName"), payload.get("content"));
             roomRepository.save(room);
